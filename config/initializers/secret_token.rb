9fc739d2374259aa6fa15081da057084e1fdeb12
Karl-Aksel Puulmann <macobo@ut.ee>
2013-05-22 16:00:22 +0300

    Rails app created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularMusicquiz::Application.config.secret_token = '6496b55c12e84419808ac02ccfa6a342ad64520bfa510c648562c67191334bf56f06582ed21ee999a32d6ffa3fdf74650114e78a9bd735cb93e4a8067ec57064'
